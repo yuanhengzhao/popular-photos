6f53259d273ea58d0854d8ab854c53fd41d2ff4e
yuanheng.zhao <yuanheng.zhao@gmail.com>
2017-09-19 01:01:10 -0400

    Create a Like model to handle like function; attach db migration and schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170919015611) do
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "photo_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
