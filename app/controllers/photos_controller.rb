7b012ae057f14b508f49e97440f909fc3d38b199
yuanhengzhao <yuanheng.zhao@gmail.com>
2017-09-19 16:29:49 -0400

    Add some comments; change type of variables.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,8 +2,8 @@ class PhotosController < ApplicationController
 	include ApplicationHelper
   def popular
   	# initialize a F00px client to get 100 popular photos. 
-  	@client = f00_client()
-  	response = @client.popular(rpp: 100, image_size: 3).body
+  	client = f00_client()
+  	response = client.popular(rpp: 100, image_size: 3).body
   	# In this task, photos will be an array with photo hashes. Should create photo model for it in the future.
   	@photos = JSON.parse(response)['photos']
     # returns all like records when user loged in.
@@ -12,24 +12,26 @@ class PhotosController < ApplicationController
     end
   end
 
-  # Like function.
+  # Like function. Send the vote post request every time even though the image may unliked by this user.
+  # Like/unlike status will be syncinzed to 500px server.
   def like
     @id = params[:id]
-    @client = f00_client()
-    @client.post("photos/#{@id}/vote?vote=1")
-    like = Like.find_or_create_by(user_id: session[:uid], photo_id: @id)
+    client = f00_client()
+    client.post("photos/#{@id}/vote?vote=1")
+    like = Like.find_or_create_by(user_id: session[:uid], photo_id: id)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js {}
     end
   end
 
-  # Unlike function.
+  # Unlike function. Send the vote post request every time even though the image may unliked by this user.
+  # Like/unlike status will be syncinzed to 500px server.
   def unlike
-    @id = params[:id]
-    @client = f00_client()
-    @client.delete("photos/#{@id}/vote?vote=0")
-    Like.where(user_id: session[:uid], photo_id: @id).delete_all
+    id = params[:id]
+    client = f00_client()
+    client.delete("photos/#{@id}/vote?vote=0")
+    Like.where(user_id: session[:uid], photo_id: id).delete_all
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js {}
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
+	# Get all photo_ids which are liked by a certain user.
 	def self.user_likes(user_id)
 		self.where(user_id: user_id).select("photo_id").map(&:photo_id)
 	end
